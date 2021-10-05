022a5ec57523f23ada99278693f0e7538840dd16
martacirer <91455125+martacirer@users.noreply.github.com>
2021-10-05 12:58:15 +0200

    5/10/21

---
+++ tema 2/MYFirstScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MYFirstScript : MonoBehaviour
+{
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Marta";
+    [SerializeField] private bool gameOver;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
